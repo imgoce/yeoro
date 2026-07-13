c093d5edf008c973f85d80e4b2b765879146a8ac
seungchan0810 <imgoce@gmail.com>
2026-07-13 15:44:41 +0900

    추천/코스 생성용 서비스 추가

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,11 @@
+from app.services.itinerary import (
+    WeatherRecommendationContext,
+    build_personalized_itinerary,
+    build_weather_recommendation,
+)
+
+__all__ = [
+    "WeatherRecommendationContext",
+    "build_personalized_itinerary",
+    "build_weather_recommendation",
+]
